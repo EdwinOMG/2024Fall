7590054d1ce56f687085004362d84f924caf74c5
EdwinOMG <150313733+EdwinOMG@users.noreply.github.com>
2024-09-30 15:10:50 -0400

    importing bulma and font awesome

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
+    <p>Welcome to the Vue.js app!</p>
+
+
   </main>
 </template>
