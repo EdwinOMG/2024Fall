37ecb77289f0ed94171fb57a49b151dcd42e39e4
EdwinOMG <150313733+EdwinOMG@users.noreply.github.com>
2024-10-10 14:55:21 -0400

    fixing small stuff

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import NavBar from './components/NavBar.vue';
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
@@ -8,15 +8,19 @@ import NavBar from './components/NavBar.vue';
     <NavBar />
   </header>
 
-  <RouterView />
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
 body {
   background-color: aliceblue;
-  }
+}
 
-  .container { 
-  background-color: whitesmoke;
-  }
+.container {
+  background-color: white;
+  box-shadow: drop-shadow(0 0 10px rgba(0, 0, 0, 0.8));
+  min-height: 100vh;
+}
 </style>
